de772271de495d331c0612f0b5bab39109b1df15
Abhishek Chougule <chouguleabhis@gmail.com>
2023-01-25 15:45:27 +0530

    login design

--- lib/login.dart
+++ lib/login.dart
@@ -50,27 +50,13 @@ class _MyLoginState extends State<MyLogin> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
+      
       backgroundColor: Colors.white,
       body: Stack(
         children: [
-          Container(
-              margin: const EdgeInsets.symmetric(vertical: 70,horizontal: 130),
-              child:
-              Image.network(
-                  'https://scontent.fpnq7-3.fna.fbcdn.net/v/t39.30808-6/298901972_439781191497646_6645786026494038575_n.png?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=vymLlBzEmVQAX889w05&_nc_ht=scontent.fpnq7-3.fna&oh=00_AfDJ5lCxNWDSBEGU39KLjABcNin_ZgdhizPWRHS3qbvrDw&oe=63D66393',
-                  fit: BoxFit.cover,
-                  
-                ),
-            ),
-          const SizedBox(
-            height: 60,
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 35, top: 50),
-          ),
                  Container(
                    padding: EdgeInsets.only(
-                       top: MediaQuery.of(context).size.height * 0.3),
+                       top: MediaQuery.of(context).size.height * 0.2),
                    child: Column(
                      crossAxisAlignment: CrossAxisAlignment.start,
                      children: [
@@ -78,14 +64,22 @@ class _MyLoginState extends State<MyLogin> {
                          margin: const EdgeInsets.only(left: 35, right: 35),
                          child: Column(
                            children: [
-                             const Text("Log in",
-                           style: TextStyle(
-                             height: 1.5,
-                             fontSize: 25,
-                             color: Colors.black,
-                              ),),
+                            Image.network(
+                  'https://scontent.fpnq7-3.fna.fbcdn.net/v/t39.30808-6/298901972_439781191497646_6645786026494038575_n.png?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=vymLlBzEmVQAX889w05&_nc_ht=scontent.fpnq7-3.fna&oh=00_AfDJ5lCxNWDSBEGU39KLjABcNin_ZgdhizPWRHS3qbvrDw&oe=63D66393',
+                  fit: BoxFit.cover,
+                  width: 80,
+                ),
+                const SizedBox(
+                               height: 10, //<-- SEE HERE
+                             ),
+                             Image.network(
+                  'https://erpnext.com/files/erpnext-logo-blue-v2.png',
+                  fit: BoxFit.cover,
+                  width: 120,
+                  
+                ),
                              const SizedBox(
-                               height: 20, //<-- SEE HERE
+                               height: 40, //<-- SEE HERE
                              ),
                              TextField(
                                style: const TextStyle(color: Colors.black),
@@ -93,9 +87,10 @@ class _MyLoginState extends State<MyLogin> {
                                
                                controller:curlController,
                                decoration: InputDecoration(
+
                                    fillColor: Colors.grey.shade100,
                                    filled: true,
-                                   
+                                   prefixIcon: const Icon(Icons.add_link), 
                                    hintText: "URL",
                                    border: OutlineInputBorder(
                                      borderRadius: BorderRadius.circular(10),
@@ -106,9 +101,11 @@ class _MyLoginState extends State<MyLogin> {
                              ),
 
                              TextField(
+                              
                                style: const TextStyle(color: Colors.black),
                                controller: emailController,
                                decoration: InputDecoration(
+                                   prefixIcon: const Icon(Icons.email_outlined), 
                                    fillColor: Colors.grey.shade100,
                                    filled: true,
                                    hintText: "Email",
@@ -124,6 +121,7 @@ class _MyLoginState extends State<MyLogin> {
                                obscureText: true,
                                controller: passwordController,
                                decoration: InputDecoration(
+                                   prefixIcon: const Icon(Icons.password), 
                                    fillColor: Colors.grey.shade100,
                                    filled: true,
                                    hintText: "Password",
@@ -137,13 +135,10 @@ class _MyLoginState extends State<MyLogin> {
                          SizedBox(
                            width: 200, // <-- Your width
                            height: 50,
-                            child: MaterialButton(
-                              padding: const EdgeInsets.symmetric(vertical: 4,horizontal: 10),
-                              materialTapTargetSize: MaterialTapTargetSize.padded,
-                              color: Colors.blue,
-                              height: 50,
-                              onPressed: () {                            
-                               usrLogin(curlController.text.toString(),emailController.text.toString(),passwordController.text.toString());
+                            child: ElevatedButton.icon(
+                              
+  onPressed: () {
+    usrLogin(curlController.text.toString(),emailController.text.toString(),passwordController.text.toString());
                                Timer(const Duration(seconds: 4),() {
                                  sCode=loginStatus.toString();
                                });
@@ -174,9 +169,15 @@ class _MyLoginState extends State<MyLogin> {
                                  const SnackBar(content: Text('Please Enter correct Username or Password !')));
                                }
                                });
-                              },
-                              child: const Text('Log in'),
-                            ),
+  },
+  
+  label: Text('Login',style:TextStyle(fontSize: 20)), 
+  icon: Icon( // <-- Icon
+    Icons.login_rounded,
+    size: 30.0,
+  ),
+), 
+                            
 
 
                          ),
