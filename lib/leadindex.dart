a1d35d8b251d88e8b88e849ba3fc925cf209d90e
Abhishek Chougule <chouguleabhis@gmail.com>
2023-01-27 14:44:42 +0530

    display lead data

--- lib/leadindex.dart
+++ lib/leadindex.dart
@@ -3,6 +3,55 @@ import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:quantbit_crm/lead.dart.';
 import 'package:quantbit_crm/pick_lead.dart';
 import 'package:quantbit_crm/side.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:quantbit_crm/login.dart' as login;
+
+var temp;
+var object;
+Future<Data> fetchData() async {
+ var headers = {
+  'Authorization': 'token da8dde973368af3:f584b09f290bab9',
+  'Cookie': 'full_name=Guest; sid=Guest; system_user=no; user_id=Guest; user_image='
+};
+var email=login.emailid;
+var request = http.Request('GET', Uri.parse('https://demo.erpdata.in/api/resource/User/$email'));
+
+request.headers.addAll(headers);
+
+http.StreamedResponse response = await request.send();
+
+if (response.statusCode == 200) {
+ await response.stream.bytesToString().then((value) {
+  temp=value;
+  
+                  print(value);
+                  });
+  //print(await response.stream.bytesToString());
+
+}
+else {
+  print(response.reasonPhrase);
+}
+return Data.fromJson(jsonDecode(request.body));
+}
+
+class Data {
+  final String name;
+  final String data;
+
+  const Data({
+    required this.data,
+    required this.name,
+  });
+
+  factory Data.fromJson(Map<String, dynamic> json) {
+    return Data(
+      data: json['data'],
+      name: json['name'],
+    );
+  }
+}
 
 class index extends StatefulWidget {
   const index({super.key});
@@ -13,6 +62,11 @@ class index extends StatefulWidget {
 }
 
 class indexState extends State<index> {
+  @override
+void initState() {
+  super.initState();
+  fetchData();
+}
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -45,6 +99,7 @@ class indexState extends State<index> {
             ]),
         body: new Column(
           children: <Widget>[
+            Text(temp),
             // new Number(),
             // new Keyboard(),
           ],
