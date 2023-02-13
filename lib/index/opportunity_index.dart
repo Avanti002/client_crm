b699b4adfcd97b1cda346261af860ebc18380b72
Sohampawar17 <sohampawar7575@gmail.com>
2023-02-13 10:35:32 +0530

    update opportunity  index

--- lib/index/opportunity_index.dart
+++ lib/index/opportunity_index.dart
@@ -7,28 +7,23 @@ import 'package:quantbit_crm/login.dart' as login;
 import 'package:quantbit_crm/accessToken.dart' as at;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
-String accessToken = at.tokenAccess;
-String curl = login.custUrl;
+String accessToken=at.tokenAccess;
+String curl=login.custUrl;
 int _selectedIndex = 0;
-List lst = [];
-String next = "";
-String oppoind = "";
-String tech = "";
+List lst=[];
+String next="";
+String oppoind="";
+String tech="";
 Future<List<Data>> fetchOppolist() async {
-  List<Data> list = [];
-  var httpsUri = Uri(
-      scheme: 'https',
-      host: '$curl',
-      path: '/api/resource/Opportunity',
-      query: 'fields=["title"]');
-  var res = await http.get(httpsUri, headers: {
+  List<Data> list=[];
+  var httpsUri = Uri(scheme: 'https',host: '$curl',path: '/api/resource/Opportunity',query:'fields=["title"]');
+  var res = await http
+      .get(httpsUri,headers: {
     'Authorization': '$accessToken',
-    'Cookie':
-        'full_name=Guest; sid=Guest; system_user=no; user_id=Guest; user_image='
+    'Cookie': 'full_name=Guest; sid=Guest; system_user=no; user_id=Guest; user_image='
   });
   if (res.statusCode == 200) {
-    lst = json.decode(res.body)["data"] as List;
+    lst=json.decode(res.body)["data"] as List;
     fetchOppolist();
 // var obj=json.decode(res.body);
 // var rest=obj["data"] as List;
@@ -36,17 +31,12 @@ Future<List<Data>> fetchOppolist() async {
   }
   return list;
 }
-
 Future<Data> fetchOpponame() async {
   var headers = {
     'Authorization': 'token da8dde973368af3:f584b09f290bab9',
-    'Cookie':
-        'full_name=Guest; sid=Guest; system_user=no; user_id=Guest; user_image='
+    'Cookie': 'full_name=Guest; sid=Guest; system_user=no; user_id=Guest; user_image='
   };
-  var request = http.Request(
-      'GET',
-      Uri.parse(
-          'https://demo.erpdata.in/api/resource/Opportunity?filters=[["title","=","$tech"]]'));
+  var request = http.Request('GET', Uri.parse('https://demo.erpdata.in/api/resource/Opportunity?filters=[["title","=","$tech"]]'));
 
   request.headers.addAll(headers);
 
@@ -54,16 +44,15 @@ Future<Data> fetchOpponame() async {
 
   if (response.statusCode == 200) {
     await response.stream.bytesToString().then((value) {
-      next = value;
-      // print(next);
 
-      oppoind = next
-          .toString()
-          .substring(18)
-          .replaceAll(RegExp('[^A-Za-z0-9-  \t]'), '');
-      print(oppoind);
+      next=value;
+     // print(next);
+
+      oppoind=next.toString().substring(18).replaceAll(RegExp('[^A-Za-z0-9-  \t]'), '');
+     print(oppoind);
     });
-  } else {
+  }
+  else {
     print(response.reasonPhrase);
   }
   return Data.fromJson(jsonDecode(request.body));
@@ -76,6 +65,7 @@ class Data {
   const Data({
     required this.data,
     required this.name,
+
   });
 
   factory Data.fromJson(Map<String, dynamic> json) {
@@ -97,8 +87,7 @@ class Opportunityindex extends StatefulWidget {
 class _Opportunityindex extends State<Opportunityindex> {
   @override
   void initState() {
-    setState(() {
-      fetchOppolist();
+    setState(() {fetchOppolist();
       //gpt.fetchContactind();
     });
     super.initState();
@@ -109,44 +98,41 @@ class _Opportunityindex extends State<Opportunityindex> {
   Widget build(BuildContext context) {
     return Scaffold(
         drawer: myDrawer(context),
-        appBar: AppBar(title: Text(widget.title), actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 20.0),
-            child: GestureDetector(
-              onTap: () {},
-              child: Icon(
-                Icons.search,
-                size: 26.0,
+        appBar: AppBar(
+            title: Text(widget.title),
+
+            actions: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.search,
+                    size: 26.0,
+                  ),
+                ),
               ),
-            ),
-          ),
-        ]),
-        body: ListView.builder(
-            itemCount: lst.length,
-            itemBuilder: ((context, position) {
-              return Card(
-                child: ListTile(
-                    title: Text((lst[position].toString())
-                        .substring(8)
-                        .replaceAll(RegExp('[^A-Za-z  \t]'), '')),
-                    selected: position == _selectedIndex,
-                    onTap: () {
-                      setState(() {
-                        _selectedIndex = position;
-                        tech = (lst[position].toString())
-                            .substring(8)
-                            .replaceAll(RegExp('[^A-Za-z  \t]'), '');
-                        fetchOpponame();
-                        print(tech);
-                      });
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DisplayOppo(title: ''),
-                          ));
-                    }),
-              );
-            })),
+            ]),
+        body: ListView.builder(itemCount: lst.length,itemBuilder: ((context,position) {
+          return Card(
+            child: ListTile(title: Text((lst[position].toString()).substring(8).replaceAll(RegExp('[^A-Za-z  \t]'), '')),
+                selected: position == _selectedIndex,onTap: () {
+                  setState(() {
+
+                    _selectedIndex = position;
+                    tech=(lst[position].toString()).substring(8).replaceAll(RegExp('[^A-Za-z  \t]'), '');
+                    fetchOpponame();
+                   print(tech);
+                  }
+                  );
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) =>DisplayOppo(title: ''),)
+                  );
+                }),
+
+          );
+        })),
         floatingActionButton: SpeedDial(
           animatedIcon: AnimatedIcons.add_event,
           children: [
@@ -166,8 +152,8 @@ class _Opportunityindex extends State<Opportunityindex> {
                   context,
                   MaterialPageRoute(
                       builder: (context) => Opportunity(
-                            title: '',
-                          )),
+                        title: '',
+                      )),
                 );
               },
             ),
